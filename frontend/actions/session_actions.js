60ea6fdf7b9c7b1a0577f645ceea9f021083f7d7
alex <alexdg316@gmail.com>
2020-05-06 21:31:07 -0700

    Create errors reducer

--- frontend/actions/session_actions.js
+++ frontend/actions/session_actions.js
@@ -2,6 +2,7 @@ import * as SessionUtil from '../utils/session_utils'
 export const RECEIVE_CURRENT_USER = "RECEIVE_CURRENT_USER"
 export const LOGOUT_CURRENT_USER = "LOGOUT_CURRENT_USER"
 export const RECEIVE_ERRORS = "RECEIVE_ERRORS"
+export const REMOVE_ERRORS = "REMOVE_ERRORS"
 
 const receiveCurrentUser = user => ({
     type: RECEIVE_CURRENT_USER,
@@ -15,6 +16,10 @@ const logoutCurrentUser = () => ({
 const receiveErrors = errors => ({
     type: RECEIVE_ERRORS,
     errors
+})
+
+export const removeErrors = () => ({
+    type: REMOVE_ERRORS
 })
 
 export const loginUser = user => dispatch => SessionUtil.login(user)
