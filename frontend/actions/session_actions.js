ed29ec25797dfebc73876a87b4419d731623f773
alex <alexdg316@gmail.com>
2020-05-06 21:13:23 -0700

    Create session actions

---
+++ frontend/actions/session_actions.js
@@ -0,0 +1,29 @@
+import * as SessionUtil from '../utils/session_utils'
+export const RECEIVE_CURRENT_USER = "RECEIVE_CURRENT_USER"
+export const LOGOUT_CURRENT_USER = "LOGOUT_CURRENT_USER"
+export const RECEIVE_ERRORS = "RECEIVE_ERRORS"
+
+const receiveCurrentUser = user => ({
+    type: RECEIVE_CURRENT_USER,
+    user
+})
+
+const logoutCurrentUser = () => ({
+    type: LOGOUT_CURRENT_USER
+})
+
+const receiveErrors = errors => ({
+    type: RECEIVE_ERRORS,
+    errors
+})
+
+export const loginUser = user => dispatch => SessionUtil.login(user)
+    .then(user => dispatch(receiveCurrentUser(user)), 
+        error => dispatch(receiveErrors(error.responseJSON))
+    )
+
+export const logoutUser = () => dispatch => SessionUtil.logout()
+    .then(() => dispatch(logoutCurrentUser))
+
+
+
