934fde31a5ba99322a442c0c16a4a431bbdd1bee
alex <alexdg316@gmail.com>
2020-05-06 21:05:28 -0700

    Create session utils

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,10 @@
 class ApplicationController < ActionController::Base
 
     helper_method :current_user, :logged_in?
-    
+
     def current_user
         return nil unless session[:session_token]
-        @current_user ||= User.find_by(session[:session_token])
+        @current_user ||= User.find_by(session_token: session[:session_token])
     end
 
     def logged_in?
--- frontend/bucket_list.jsx
+++ frontend/bucket_list.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { login, logout } from './utils/session_utils'
 
 document.addEventListener('DOMContentLoaded', () => {
     const root = document.getElementById('root')
     ReactDOM.render(<h1>Working</h1>, root)
+
+    window.login = login
+    window.logout = logout
 })
