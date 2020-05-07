a4a5ed6d5c771e2a77d12eb04e9df5653f104484
alex <alexdg316@gmail.com>
2020-05-06 20:32:29 -0700

    Create session controller

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,11 +1,26 @@
 class Api::SessionsController < ApplicationController
     
     def create
-        
+        @user = User.find_by_credentials(
+            params[:user][:username],
+            params[:user][:password]
+        )
+
+        if @user
+            login(@user)
+            render 'api/users/show'
+        else 
+            render json: ["Invalid username or password"], status: 422
+        end
     end
 
     def destroy
-        
+        logout
+        if !logged_in?
+            render json: {}
+        else  
+            render json: ["Unable to logout"], status: 404
+        end
     end
 
 end
